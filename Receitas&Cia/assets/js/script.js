5d580d37db1102704e4beacd9f490a2bb0782091
João Barbosa <contato.jsbarbosa@gmail.com>
2022-11-17 20:51:55 -0300

    Atualização do projeto- versão 2

--- Receitas&Cia/assets/js/script.js
+++ Receitas&Cia/assets/js/script.js
@@ -1,8 +1,54 @@
-function mouseOver() {
-  var x = document.getElementById("display-b");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+var modal = document.getElementById("id01");
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-}
+};
+
+var modal2 = document.getElementById("id02");
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
+
+var modal3 = document.getElementById("id03");
+window.onclick = function (event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+};
+
+var modal4 = document.getElementById("id04");
+window.onclick = function (event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+};
+
+var modal5 = document.getElementById("id05");
+window.onclick = function (event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+};
+
+var modal6 = document.getElementById("id06");
+window.onclick = function (event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+};
+
+var modal7 = document.getElementById("id07");
+window.onclick = function (event) {
+  if (event.target == modal7) {
+    modal7.style.display = "none";
+  }
+};
+var modal8 = document.getElementById("id08");
+window.onclick = function (event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+};
